4b964861201b3c597367c323c98b01195b48ea91
Pranay Jumde <pranay.jumde@qodequay.com>
2025-12-10 00:59:58 +0530

    Added Web Client Setup in here

---
+++ 14-Inter-Service-Communication/consumer/src/main/java/com/example/consumer/webclient/WebClientConfig.java
@@ -0,0 +1,10 @@
+//package com.example.consumer.webclient;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.reactive.function.client.WebClient;
+//
+//@Configuration
+//public class WebClientConfig {
+//
+//    public WebClient webClient(WebClientB)
+//}
