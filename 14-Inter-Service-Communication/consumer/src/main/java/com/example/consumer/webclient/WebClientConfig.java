7c8c4668727f5dfa28ac8b3c47baf2a9f8e9c2d8
Pranay Jumde <pranay.jumde@qodequay.com>
2025-12-10 01:08:34 +0530

    Added Web Client Setup with config and service in here

--- 14-Inter-Service-Communication/consumer/src/main/java/com/example/consumer/webclient/WebClientConfig.java
+++ 14-Inter-Service-Communication/consumer/src/main/java/com/example/consumer/webclient/WebClientConfig.java
@@ -1,10 +1,17 @@
-//package com.example.consumer.webclient;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.reactive.function.client.WebClient;
-//
-//@Configuration
-//public class WebClientConfig {
-//
-//    public WebClient webClient(WebClientB)
-//}
+package com.example.consumer.webclient;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfig {
+
+    private static final String PROVIDER_URL = "http://localhost:8081";
+
+    @Bean
+    public WebClient webClient(WebClient.Builder builder) {
+        return builder.baseUrl(PROVIDER_URL)
+                .build();
+    }
+}
