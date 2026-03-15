43c199c6851399d7285704735767b381cda7fbae
Pranay Jumde <pranay.jumde@qodequay.com>
2026-03-15 19:01:19 +0530

    Done with Http Interface. also we can do it with Rest Template, client, web client also

--- 15-Service-Discovery-With-Eureka/consumer/src/main/java/com/example/consumer/httpinterface/HttpInterfaceConfig.java
+++ 15-Service-Discovery-With-Eureka/consumer/src/main/java/com/example/consumer/httpinterface/HttpInterfaceConfig.java
@@ -2,17 +2,17 @@ package com.example.consumer.httpinterface;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestClient;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.client.support.RestTemplateAdapter;
-import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.web.reactive.function.client.support.WebClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 import org.springframework.web.util.DefaultUriBuilderFactory;
 
 @Configuration
 public class HttpInterfaceConfig {
+
+//    private static final String PROVIDER_URL = "http://localhost:8081";
+
+    private static final String PROVIDER_URL = "http://provider";
 
 //    @Bean
 //    public ProviderHttpInterface webClientHttpInterface() {
@@ -44,11 +44,19 @@ public class HttpInterfaceConfig {
 //        return service;
 //    }
 
-    @Bean
-    public ProviderHttpInterface restTemplateHttpInterface() {
-        RestTemplate restTemplate = new RestTemplate();
+    // Why this is commented cause
+    // Rest template Bean is already present in app so thats why we used that
+    // else we need to uncomment this
 
-        restTemplate.setUriTemplateHandler(new DefaultUriBuilderFactory("http://localhost:8081"));
+    //    @Bean
+//    @LoadBalanced
+//    public RestTemplate restTemplate() {
+//        return new RestTemplate();
+//    }
+    @Bean
+    public ProviderHttpInterface restTemplateHttpInterface(RestTemplate restTemplate) {
+
+        restTemplate.setUriTemplateHandler(new DefaultUriBuilderFactory(PROVIDER_URL));
 
         RestTemplateAdapter adapter = RestTemplateAdapter.create(restTemplate);
 
