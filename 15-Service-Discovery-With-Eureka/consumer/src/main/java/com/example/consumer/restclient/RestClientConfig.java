84e850f70208a4223e562b134b38753634323e4d
Pranay Jumde <pranay.jumde@qodequay.com>
2026-03-15 18:33:42 +0530

    Done with Rest Client. Rest Client need to add Builder bean and upon that add load balanced annotation

--- 15-Service-Discovery-With-Eureka/consumer/src/main/java/com/example/consumer/restclient/RestClientConfig.java
+++ 15-Service-Discovery-With-Eureka/consumer/src/main/java/com/example/consumer/restclient/RestClientConfig.java
@@ -1,5 +1,6 @@
 package com.example.consumer.restclient;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
@@ -7,7 +8,14 @@ import org.springframework.web.client.RestClient;
 @Configuration
 public class RestClientConfig {
 
-    private static final String PROVIDER_URL = "http://localhost:8081";
+    //    private static final String PROVIDER_URL = "http://localhost:8081";
+    private static final String PROVIDER_URL = "http://provider";
+
+    @Bean
+    @LoadBalanced
+    public RestClient.Builder loadBalancedRestClientBuilder(){
+        return RestClient.builder();
+    }
 
     @Bean
     public RestClient restClient(RestClient.Builder restClient) {
